84cc3e8d1c29a5cafc1c2f961acaaa131ba3c5c0
Daniel Halperin <daniel@halper.in>
2015-09-08 03:58:47 -0700

    revert to first working reachable

--- src/main/java/edu/washington/escience/lca/LCA.java
+++ src/main/java/edu/washington/escience/lca/LCA.java
@@ -15,13 +15,11 @@ import com.google.cloud.dataflow.sdk.options.PipelineOptions;
 import com.google.cloud.dataflow.sdk.options.PipelineOptionsFactory;
 import com.google.cloud.dataflow.sdk.options.Validation.Required;
 import com.google.cloud.dataflow.sdk.transforms.DoFn;
-import com.google.cloud.dataflow.sdk.transforms.Flatten;
 import com.google.cloud.dataflow.sdk.transforms.KvSwap;
 import com.google.cloud.dataflow.sdk.transforms.ParDo;
 import com.google.cloud.dataflow.sdk.transforms.View;
 import com.google.cloud.dataflow.sdk.values.KV;
 import com.google.cloud.dataflow.sdk.values.PCollection;
-import com.google.cloud.dataflow.sdk.values.PCollectionList;
 import com.google.cloud.dataflow.sdk.values.PCollectionTuple;
 import com.google.cloud.dataflow.sdk.values.PCollectionView;
 import com.google.cloud.dataflow.sdk.values.TupleTag;
@@ -117,30 +115,28 @@ public class LCA {
 							}
 						}));
 
-
 		PCollection<KV<Integer, Reachable>> delta0 = reachable0;
 
-		/* The setup for every loop: */
+
+
+		PCollection<KV<Integer, Reachable>> reachable = reachable0;
 		PCollection<KV<Integer, Reachable>> delta = delta0;
-		PCollectionList<KV<Integer, Reachable>> allDeltas = PCollectionList.of(delta);
-		PCollection<KV<Integer, Reachable>> reachable = allDeltas.apply(Flatten.pCollections());
 
 		for (int i = 1; i < 30; ++i) {
 			TupleTag<KV<Integer, Reachable>> reachableInTag = new TupleTag<KV<Integer, Reachable>>(){};
+			TupleTag<KV<Integer, Reachable>> reachableOutTag = new TupleTag<KV<Integer, Reachable>>(){};
 			TupleTag<KV<Integer, Integer>> graphTag = new TupleTag<KV<Integer, Integer>>(){};
 			TupleTag<KV<Integer, Reachable>> deltaInTag = new TupleTag<KV<Integer, Reachable>>(){};
-
-			ReachableStep step = new ReachableStep(reachableInTag, deltaInTag, graphTag,
+			TupleTag<KV<Integer, Reachable>> deltaOutTag = new TupleTag<KV<Integer, Reachable>>(){};
+			ReachableStep step = new ReachableStep(reachableInTag, deltaInTag, graphTag, reachableOutTag, deltaOutTag,
 					i, options.getOutputDirectory(), false /* debug */);
 
-			PCollection<KV<Integer,Reachable>> oneHopResults = step.apply(
-					PCollectionTuple.of(graphTag, graphIn)
+			PCollectionTuple oneHopResults = step.apply(PCollectionTuple.of(graphTag, graphIn)
 					.and(reachableInTag, reachable)
 					.and(deltaInTag, delta));
 
-			delta = oneHopResults;
-			allDeltas = allDeltas.and(delta);
-			reachable = allDeltas.apply(Flatten.pCollections());
+			reachable = oneHopResults.get(reachableOutTag);
+			delta = oneHopResults.get(deltaOutTag);
 		}
 
 		reachable
--- src/main/java/edu/washington/escience/lca/ReachableStep.java
+++ src/main/java/edu/washington/escience/lca/ReachableStep.java
@@ -14,14 +14,17 @@ import com.google.cloud.dataflow.sdk.values.KV;
 import com.google.cloud.dataflow.sdk.values.PCollection;
 import com.google.cloud.dataflow.sdk.values.PCollectionTuple;
 import com.google.cloud.dataflow.sdk.values.TupleTag;
+import com.google.cloud.dataflow.sdk.values.TupleTagList;
 import com.google.common.collect.Sets;
 
 @SuppressWarnings("serial")
-public class ReachableStep extends PTransform<PCollectionTuple, PCollection<KV<Integer, Reachable>>> {
+public class ReachableStep extends PTransform<PCollectionTuple, PCollectionTuple> {
 	private static final long serialVersionUID = 1L;
 	private final TupleTag<KV<Integer, Reachable>> reachableInTag;
 	private final TupleTag<KV<Integer, Reachable>> deltaInTag;
 	private final TupleTag<KV<Integer, Integer>> graphTag;
+	private final TupleTag<KV<Integer, Reachable>> reachableOutTag;
+	private final TupleTag<KV<Integer, Reachable>> deltaOutTag;
 	private final int step;
 	private final String outputDirectory;
 	private final boolean debug;
@@ -30,19 +33,23 @@ public class ReachableStep extends PTransform<PCollectionTuple, PCollection<KV<I
 			TupleTag<KV<Integer, Reachable>> reachableInTag,
 			TupleTag<KV<Integer, Reachable>> deltaInTag,
 			TupleTag<KV<Integer, Integer>> graphTag,
+			TupleTag<KV<Integer, Reachable>> reachableOutTag,
+			TupleTag<KV<Integer, Reachable>> deltaOutTag,
 			int step,
 			String outputDirectory,
 			boolean debug) {
 		this.reachableInTag = reachableInTag;
 		this.deltaInTag = deltaInTag;
 		this.graphTag = graphTag;
+		this.reachableOutTag = reachableOutTag;
+		this.deltaOutTag = deltaOutTag;
 		this.step = step;
 		this.outputDirectory = outputDirectory;
 		this.debug = debug;
 	}
 
 	@Override
-	public PCollection<KV<Integer, Reachable>> apply(PCollectionTuple input) {
+	public PCollectionTuple apply(PCollectionTuple input) {
 		PCollection<KV<Integer, Reachable>> oldDelta = input.get(deltaInTag);
 		PCollection<KV<Integer, Integer>> graphOut = input.get(graphTag);
 
@@ -78,10 +85,11 @@ public class ReachableStep extends PTransform<PCollectionTuple, PCollection<KV<I
 		TupleTag<Reachable> keyedJoinResultTag = new TupleTag<Reachable>(){};
 		PCollection<KV<Integer, Reachable>> oldReachable = input.get(reachableInTag);
 
-		PCollection<KV<Integer, Reachable>> output = KeyedPCollectionTuple.of(keyedReachableTag, oldReachable)
+		PCollectionTuple output = KeyedPCollectionTuple.of(keyedReachableTag, oldReachable)
 				.and(keyedJoinResultTag, oneHop)
 				.apply("CoGroupReachableAndDelta_"+step, CoGroupByKey.<Integer>create())
 				.apply("UpdateReachableAndDelta_"+step, ParDo
+						.withOutputTags(reachableOutTag, TupleTagList.of(deltaOutTag))
 						.of(new DoFn<KV<Integer, CoGbkResult>, KV<Integer, Reachable>>(){
 							private static final long serialVersionUID = 1L;
 
@@ -91,24 +99,30 @@ public class ReachableStep extends PTransform<PCollectionTuple, PCollection<KV<I
 								Integer source = result.getKey();
 								CoGbkResult join = result.getValue();
 								Set<Integer> alreadyKnown = Sets.newHashSet();
+								Set<Reachable> newReachable = Sets.newHashSet(join.getAll(keyedReachableTag));
 								Set<Reachable> newDelta = Sets.newHashSet();
 								// Record all the destinations this src can already.
-								for (Reachable r : join.getAll(keyedReachableTag)) {
+								for (Reachable r : newReachable) {
 									Verify.verify(alreadyKnown.add(r.dst), "Adding already known destination %s to src %s", r.dst, source);
 								}
 								for (Reachable r : join.getAll(keyedJoinResultTag)) {
 									if (alreadyKnown.add(r.dst)) {
+										newReachable.add(r);
 										newDelta.add(r);
 									}
 								}
+								// Emit all the new reachable set, which includes the new results.
+								for (Reachable r : newReachable) {
+									c.output(KV.of(source, r));
+								}
 								// Emit all the new delta set, which has been uniquified and deltaed.
 								for (Reachable d : newDelta) {
-									c.output(KV.of(source, d));
+									c.sideOutput(deltaOutTag, KV.of(source, d));
 								}
 							}}));
 
 		if (debug) {
-			output
+			output.get(deltaOutTag)
 			.apply("StringifyDelta_" + step, ParDo.of(new StringifyReachable()))
 			.apply("OutputDelta_" + step, TextIO.Write.to(outputDirectory + "/delta" + step).withSuffix(".txt"));
 		}
